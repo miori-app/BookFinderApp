086e776dfa3ae1e67ad9ac53d35abdadeadcda19
iammiori <mioriii@naver.com>
2022-04-21 18:24:34 +0900

    feat - cell 에 띄어줄 데이터 모델 구현

---
+++ BookFinderApp/Model/CellDataModel.swift
@@ -0,0 +1,15 @@
+//
+//  CellDataModel.swift
+//  BookFinderApp
+//
+//  Created by miori Lee on 2022/04/21.
+//
+
+import Foundation
+
+struct CellDataModel {
+    let thumbnailURL : String?
+    let title : String?
+    let author : String?
+    let datetime : String?
+}
