d74f5e66136afa2aefa55ea6e9afec746d93ca10
Mi Yeon Lee <46439995+iammiori@users.noreply.github.com>
2022-04-24 16:07:01 +0900

    feat - 검색결과 총 아이템 수 헤어뷰에 띄우기 (뷰,뷰모델,바인딩) (#13)
    * feat - 총 검색결과수 label 을 포함한 테이블뷰의 헤더뷰 구현
    
    * feat - 테이블 뷰의 헤더뷰를 구현한 헤더뷰로 지정
    
    * feat - response 결과 값을 총아이템수와 맵핑 + 맵핑한 값 내보내기

---
+++ BookFinderApp/View/BookTableViewHeaderView.swift
@@ -0,0 +1,52 @@
+//
+//  BookTableViewHeaderView.swift
+//  BookFinderApp
+//
+//  Created by miori Lee on 2022/04/22.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+
+class BookTableViewHeaderView : UITableViewHeaderFooterView {
+    
+    let totalCountLabel = UILabel()
+    let bottomBorder = UIView()
+    
+    let disposeBag = DisposeBag()
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        
+        setAttribute()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func setAttribute() {
+        totalCountLabel.font = .systemFont(ofSize: 10, weight: .medium)
+        totalCountLabel.text = "📚 검색된 결과 수 :"
+        bottomBorder.backgroundColor = .systemGray3
+    }
+    
+    private func setLayout() {
+        [totalCountLabel, bottomBorder].forEach {addSubview($0)}
+        
+        totalCountLabel.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview().offset(12)
+        }
+        
+        bottomBorder.snp.makeConstraints {
+            $0.top.equalTo(totalCountLabel.snp.bottom)
+            $0.leading.trailing.bottom.equalToSuperview()
+            $0.height.equalTo(0.5)
+        }
+    }
+}
