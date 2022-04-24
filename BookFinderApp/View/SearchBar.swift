c6992f43c1d534257587699c32aaeba879c6a050
iammiori <mioriii@naver.com>
2022-04-25 05:37:49 +0900

    refac - placeholder 값 model에 지정한 데이터로 변경

--- BookFinderApp/View/SearchBar.swift
+++ BookFinderApp/View/SearchBar.swift
@@ -14,25 +14,25 @@ import RxCocoa
 class SearchBar : UISearchController {
     
     let disposeBag = DisposeBag()
-    
+
     override init(searchResultsController: UIViewController?) {
         super.init(searchResultsController: nil)
         setSearchBar()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     private func setSearchBar() {
-        self.searchBar.placeholder = "검색할 책 이름을 적어주세요"
+        self.searchBar.placeholder = LabelText.searchBarPlaceholder
     }
-    
+
     func bind(_ viewModel : SearchBarViewModel) {
         self.searchBar.rx.text
             .bind(to: viewModel.inputQuery)
             .disposed(by: disposeBag)
-        
+
         self.searchBar.rx.searchButtonClicked
             .asObservable()
             .bind(to: viewModel.searchBtnTapped)
